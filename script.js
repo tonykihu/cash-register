91ecdc45ab110149602bb5e5d41322901c82a0f2
tonykihu <{wamuthumbikihu@gmail.com}>
2025-01-30 21:54:12 +0300

    CR

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ let cid = [
 purchaseButton.addEventListener('click', () => {
   const customerCashValue = customerCash.value;
   const change = customerCashValue - price;
+  //changeDue.textContent = `Change: $${change}`
   if (customerCashValue === "") {
     alert("Please provide cash amount");
   } else if (customerCashValue < price) {
     alert("Customer does not have enough money to purchase the item");
-  } else {
-    changeDue.textContent = `Change: $${change}`;
+  } else if (customerCashValue == price){
+    changeDue.textContent = "No change due - customer paid with exact cash";
   }
-})
+});
