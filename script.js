799db23df2b30166de0a4dd61b6b6b18b9014fd1
tonykihu <{wamuthumbikihu@gmail.com}>
2025-01-31 00:16:59 +0300

    CR

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const customerCash = document.getElementById('cash');
 const purchaseButton = document.getElementById('purchase-btn');
 const changeDue = document.getElementById('change-due');
+const drawerStatus = document.getElementById('status');
+const penny = document.getElementById('penny');
+const nickel = document.getElementById('nickel');
+const dime = document.getElementById('dime');
+const quarter = document.getElementById('quarter');
+const one = document.getElementById('one');
+const five = document.getElementById('five');
+const ten = document.getElementById('ten');
+const twenty = document.getElementById('twenty');
+const hundred = document.getElementById('hundred');
+const total = document.getElementById('total');
 
 let price = 20;
 let cid = [
@@ -15,10 +26,26 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
+let totalCid = cid.reduce((sum, [_, amount]) => sum + amount, 0);
+
+penny.innerHTML = cid[0][1];
+nickel.textContent = cid[1][1];
+dime.textContent = cid[2][1];
+quarter.textContent = cid[3][1];
+one.innerHTML = cid[4][1];
+five.textContent = cid[5][1];
+ten.textContent = cid[6][1];
+twenty.textContent = cid[7][1];
+hundred.textContent = cid[8][1];
+total.textContent = totalCid;
+
+console.log(one.textContent);
+
+
 purchaseButton.addEventListener('click', () => {
   const customerCashValue = customerCash.value;
   const change = customerCashValue - price;
-  //changeDue.textContent = `Change: $${change}`
+  changeDue.textContent = `Change: $${change}`
   if (customerCashValue === "") {
     alert("Please provide cash amount");
   } else if (customerCashValue < price) {
@@ -26,4 +53,14 @@ purchaseButton.addEventListener('click', () => {
   } else if (customerCashValue == price){
     changeDue.textContent = "No change due - customer paid with exact cash";
   }
+  
+  if (totalCid < change) {
+    drawerStatus.innerHTML = "INSUFFICIENT_FUNDS";
+  } else if (totalCid === change) {
+    drawerStatus.textContent = `CLOSED`;
+  } else {
+    drawerStatus.textContent = "OPEN";
+  }
+  
 });
+
